388f55ec6ea3c8e288e7c7dc052fa85927600396
Paul Ashour <paul.ashour@gmail.com>
2018-09-25 03:43:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ function placeOrder(cardNumber) {
   if(cardNumber === null) {
     console.log("Sorry, we don't have a credit card on file for you.");
   } else {
-    let total = total(cart);
+    let curTotal = total(cart);
     cart = [];
     return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`;
   }
